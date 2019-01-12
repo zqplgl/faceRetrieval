d434a220037ffc5280006f6873076b2f1c1c260c
zqplgl <1316315878@qq.com>
2019-01-13 01:19:24 +0800

    modify IFaceRetrieval.py

--- IFaceRetrieval.py
+++ IFaceRetrieval.py
@@ -5,7 +5,7 @@ import numpy as np
 from annoy import AnnoyIndex
 
 class FaceRetrieval:
-    def __init__(self, model_dir, retrieval_model_path=None, gpu_id=0):
+    def __init__(self, model_dir, gpu_id=0):
         deploy_file = os.path.join(model_dir,"faceretrieval/resnet50_ft.prototxt")
         assert os.path.exists(deploy_file)
 
@@ -23,10 +23,12 @@ class FaceRetrieval:
         mean_value = [91.4953,103.8827, 131.0912]
         self.__extracter = ObjTypeClassifier(deploy_file,weights_file,meanvalue=mean_value)
 
-        if retrieval_model_path:
-            assert (os.path.exists(retrieval_model_path))
-            self.__database = AnnoyIndex(self.__feature_len)
-            self.__database.load(retrieval_model_path)
+    def create_retrieval_model(self, retrieval_model_path):
+        assert (os.path.exists(retrieval_model_path))
+        retrieval_model = AnnoyIndex(self.__feature_len)
+        retrieval_model.load(retrieval_model_path)
+
+        return retrieval_model
 
     def extractFeature(self,im):
         try:
@@ -52,9 +54,9 @@ class FaceRetrieval:
         self.__database.build(2*self.__feature_len)
         self.__database.save(retrieval_model_path)
 
-    def query(self,im, k):
+    def query(self,im, k, retrieval_model):
         feature = self.extractFeature(im)
-        res = self.__database.get_nns_by_vector(feature,k,include_distances=False)
+        res = retrieval_model.get_nns_by_vector(feature,k,include_distances=False)
         return  res
 
 def buildRetrievalDatabase():
@@ -80,13 +82,15 @@ def buildRetrievalDatabase():
 
 def query():
     model_dir = "/home/zqp/install_lib/models"
-    net = FaceRetrieval(model_dir,"./123.tree")
+    retireval_model_path="./123.tree"
+    net = FaceRetrieval(model_dir)
+    retrieval_model = net.create_retrieval_model(retrieval_model_path)
 
     pic_dir = "/home/zqp/github/DllFaceDeepFeature/testimage"
     for pic_name in os.listdir(pic_dir):
         im = cv2.imread(os.path.join(pic_dir,pic_name))
 
-        res = net.query(im,5)
+        res = net.query(im,5,retrieval_model_path)
         print res
 
 
